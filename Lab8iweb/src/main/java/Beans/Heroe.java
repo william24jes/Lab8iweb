2d083dee0a0083de5a11cd993a1ab04d3822aee3
william <william24jes@hotmail.com>
2022-11-04 13:25:50 -0500

    william - daoheroes,heroes

--- Lab8iweb/src/main/java/Beans/Heroe.java
+++ Lab8iweb/src/main/java/Beans/Heroe.java
@@ -1,10 +1,86 @@
 package Beans;
 
 public class Heroe {
-    private String idHeroes;
+    private int idHeroe;
     private String nombre;
-    private String edad;
-    private String nivelInicial;
-    private String ataque;
+    private int edad;
+    private int nivel;
+    private int puntosDeExperiencia;
+    private String pareja;
+    private String genero;
+    private String clase;
+    private int ataque;
 
+    public int getIdHeroe() {
+        return idHeroe;
+    }
+
+    public void setIdHeroe(int idHeroe) {
+        this.idHeroe = idHeroe;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getPuntosDeExperiencia() {
+        return puntosDeExperiencia;
+    }
+
+    public void setPuntosDeExperiencia(int puntosDeExperiencia) {
+        this.puntosDeExperiencia = puntosDeExperiencia;
+    }
+
+    public String getPareja() {
+        return pareja;
+    }
+
+    public void setPareja(String pareja) {
+        this.pareja = pareja;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
 }
+
--- Lab8iweb/src/main/java/Daos/HeroeDao.java
+++ Lab8iweb/src/main/java/Daos/HeroeDao.java
@@ -1,4 +1,46 @@
 package Daos;
+import Beans.Heroe;
 
+import java.sql.*;
+import java.util.ArrayList;
 public class HeroeDao {
+
+    public ArrayList<Heroe> obtenerlistaHeroes(){
+        ArrayList<Heroe> listaHeroes = new ArrayList<>();
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/l6_20203368";
+        String sql = "SELECT * FROM heroes";
+
+        try(Connection connection = DriverManager.getConnection(url,"root","root");
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)){
+
+            while(rs.next()){
+                Heroe heroe = new Heroe();
+
+                heroe.setIdHeroe(rs.getInt(1));
+                heroe.setNombre(rs.getString(2));
+                heroe.setEdad(rs.getInt(3));
+                heroe.setNivel(rs.getInt(  4));
+                heroe.setPuntosDeExperiencia(rs.getInt(5));
+                heroe.setPareja(rs.getString(6));
+                heroe.setGenero(rs.getString(7));
+                heroe.setClase(rs.getString(8));
+                heroe.setAtaque(rs.getInt(9));
+
+                listaHeroes.add(heroe);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return listaHeroes;
+    }
+
+
 }
