8a95ac751c39d5b4f68abc631016faba5487534f
william <william24jes@hotmail.com>
2022-11-04 17:55:23 -0500

    william - Heroe Dao y Servlet modificados

--- Lab8iweb/src/main/java/Servlets/Servlet.java
+++ Lab8iweb/src/main/java/Servlets/Servlet.java
@@ -1,6 +1,7 @@
 package Servlets;
 
 import Beans.Enemigo;
+import Beans.Heroe;
 import Daos.EnemigoDao;
 import Daos.HeroeDao;
 import jakarta.servlet.*;
@@ -48,5 +49,66 @@ public class Servlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String accion = request.getParameter("accion");
+        HeroeDao heroeDao = new HeroeDao();
+        Heroe heroe = new Heroe();
+        RequestDispatcher requestDispatcher;
+
+        switch (accion) {
+
+            case "actualizar":
+
+                heroe.setIdHeroe(Integer.parseInt(request.getParameter("ID Heroe"))); /*colocar los parametros en los botones del jsp*/
+                heroe.setNombre(request.getParameter("Nombre"));
+                heroe.setEdad(Integer.parseInt(request.getParameter("Apellidos")));
+                heroe.setNivel(Integer.parseInt(request.getParameter("Correo PUCP")));
+                heroe.setPuntosDeExperiencia(Integer.parseInt(request.getParameter("DNI")));
+                heroe.setPareja(request.getParameter("Celular"));
+                /*colocar if-else para genero y clase despues*/
+                heroe.setGenero(request.getParameter("Categoría"));
+                heroe.setClase(request.getParameter("Rol"));
+                heroe.setAtaque(Integer.parseInt(request.getParameter("Codigo")));
+
+                heroeDao.actualizarHeroe(heroe);
+
+                response.sendRedirect(request.getContextPath() + "/AdminServlet");
+
+                break;
+
+            /*case "actualizarParcial":
+                String jobId2 = request.getParameter("jobId");
+                String jobTitle2 = request.getParameter("jobTitle");
+                String minSalaryStr2 = request.getParameter("minSalary");
+                int minSalary2 = Integer.parseInt(minSalaryStr2);
+
+                try {
+                    daoJob.actualizarParcial(jobId2, jobTitle2, minSalary2);
+                    response.sendRedirect(request.getContextPath() + "/JobServlet");
+                } catch (SQLException e) {
+                    job = daoJob.buscarPorId(jobId2);
+
+                    if (job != null) { //abro el form para editar
+                        request.setAttribute("job", job);
+                        request.setAttribute("error","El texto no puede tener mas de 255 caractéres");
+                        RequestDispatcher requestDispatcher = request.getRequestDispatcher("jobs/formEditarParcial.jsp");
+                        requestDispatcher.forward(request, response);
+                    } else { //id no encontrado
+                        response.sendRedirect(request.getContextPath() + "/JobServlet");
+                    }
+                }
+
+                break;
+
+            case "buscar":
+                String searchText = request.getParameter("searchText");
+
+                ArrayList<Usuarios> lista = daoUsuarios.buscarUsuarios(searchText);
+                request.setAttribute("lista", lista);
+
+                requestDispatcher = request.getRequestDispatcher("AdminListaUsers.jsp");
+                requestDispatcher.forward(request, response);
+
+                break;*/
+        }
     }
 }
