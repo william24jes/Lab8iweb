03618d9a9a3369ac07e92ef800c3878de51157a8
DiegoLG15 <a20200839@pucp.edu.pe>
2022-11-04 11:31:44 -0500

    Beans Enemigos

--- Lab8iweb/src/main/java/Beans/Enemigos.java
+++ Lab8iweb/src/main/java/Beans/Enemigos.java
@@ -1,4 +1,68 @@
 package Beans;
 
 public class Enemigos {
+
+    private String nombre;
+    private int clase;
+    private int ataque;
+    private int experienciaxDerrota;
+    private String objetoDejado;
+    private float probDejarObjeto;
+    private String genero;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getClase() {
+        return clase;
+    }
+
+    public void setClase(int clase) {
+        this.clase = clase;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getExperienciaxDerrota() {
+        return experienciaxDerrota;
+    }
+
+    public void setExperienciaxDerrota(int experienciaxDerrota) {
+        this.experienciaxDerrota = experienciaxDerrota;
+    }
+
+    public String getObjetoDejado() {
+        return objetoDejado;
+    }
+
+    public void setObjetoDejado(String objetoDejado) {
+        this.objetoDejado = objetoDejado;
+    }
+
+    public float getProbDejarObjeto() {
+        return probDejarObjeto;
+    }
+
+    public void setProbDejarObjeto(float probDejarObjeto) {
+        this.probDejarObjeto = probDejarObjeto;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 }
